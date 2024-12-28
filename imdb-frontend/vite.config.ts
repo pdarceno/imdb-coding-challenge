a19a419fcfe2a6e0c69641ad0ec17da12a85037a
Prosy Arceno <pdarceno@gmail.com>
2024-12-28 14:23:17 +0800

    added shadcn for card and dialog component

--- imdb-frontend/vite.config.ts
+++ imdb-frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// https://ui.shadcn.com/docs/installation/vite
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
