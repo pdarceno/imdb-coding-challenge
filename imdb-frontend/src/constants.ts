7e68f9750816fcf8a622752e48be395cc738ddb5
Prosy Arceno <pdarceno@gmail.com>
2024-12-28 15:22:31 +0800

    modified search to return on keystrokes with buffer

---
+++ imdb-frontend/src/constants.ts
@@ -0,0 +1,4 @@
+export const SEARCH_CONSTANTS = {
+  MIN_DEBOUNCE_TIME: 500, // Minimum time to wait before searching (in milliseconds)
+  MAX_DEBOUNCE_TIME: 1000, // Maximum time window for search to be valid (in milliseconds)
+} as const;
